5c4e9be61a179b82b9fb6ae67665b0434c93dc37
lollipop <liujian594@hotmail.com>
2024-09-03 20:53:10 +0800

    feat: 增加气压计的项目，做个自己的气压计

---
+++ barometer/src/main/java/com/lollipop/wear/barometer/MainActivity.kt
@@ -0,0 +1,13 @@
+
+package com.lollipop.wear.barometer
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        installSplashScreen()
+        super.onCreate(savedInstanceState)
+    }
+}
