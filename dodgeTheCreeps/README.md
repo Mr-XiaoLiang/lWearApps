7ed581422b968c5c0b701ad34d70b63b1b539cc8
lollipop <liujian594@hotmail.com>
2024-09-14 21:01:07 +0800

    feat: 跟着教程做个小游戏，练手吧

---
+++ dodgeTheCreeps/README.md
@@ -0,0 +1,3 @@
+# Dodge the Creeps 2D
+
+https://docs.godotengine.org/en/latest/getting_started/first_2d_game/index.html
---
+++ dodgeTheCreeps/src/main/java/com/lollipop/dtc/DtcApplication.kt
@@ -0,0 +1,12 @@
+package com.lollipop.dtc
+
+import android.app.Application
+
+class DtcApplication: Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+
+    }
+
+}
