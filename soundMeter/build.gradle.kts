20bf454c4fa034276c258a95f1bef482b76ce6d6
lollipop <liujian594@hotmail.com>
2024-10-07 00:22:32 +0800

    feat: 增加了一个新的模块，做一个分贝测量仪

---
+++ soundMeter/build.gradle.kts
@@ -0,0 +1,39 @@
+plugins {
+    alias(libs.plugins.android.application)
+    alias(libs.plugins.jetbrains.kotlin.android)
+}
+
+android {
+    namespace = "com.lollipop.sound"
+    compileSdk = 34
+
+    defaultConfig {
+        applicationId = "com.lollipop.sound"
+        minSdk = 28
+        targetSdk = 34
+        versionCode = 1
+        versionName = "1.0"
+
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+}
+
+dependencies {
+implementation(project(":basic"))
+}
