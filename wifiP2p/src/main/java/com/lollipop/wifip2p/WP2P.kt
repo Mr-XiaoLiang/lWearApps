6fa765e417097416fdf6ef93190e3514f55f5481
lollipop <liujian594@hotmail.com>
2024-10-20 18:57:00 +0800

    feat: 也许可以把demo先跑一下试试

--- wifiP2p/src/main/java/com/lollipop/wifip2p/WP2P.kt
+++ wifiP2p/src/main/java/com/lollipop/wifip2p/WP2P.kt
@@ -1,7 +1,11 @@
 package com.lollipop.wifip2p
 
+import android.app.Activity
+
 object WP2P {
 
-
+    fun with(activity: Activity, callback: WPActivityDelegate.Callback): WPActivityDelegate {
+        return WPActivityDelegate(activity, callback)
+    }
 
 }
