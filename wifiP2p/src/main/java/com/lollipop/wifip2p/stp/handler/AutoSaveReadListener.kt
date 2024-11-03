3ab9eb3d823297bc0cfe19c0133d34dd85de9914
lollipop <liujian594@hotmail.com>
2024-11-03 19:47:44 +0800

    feat: 从下往上写有点头疼。明天开始从上往下写

--- wifiP2p/src/main/java/com/lollipop/wifip2p/stp/handler/AutoSaveReadListener.kt
+++ wifiP2p/src/main/java/com/lollipop/wifip2p/stp/handler/AutoSaveReadListener.kt
@@ -2,9 +2,10 @@ package com.lollipop.wifip2p.stp.handler
 
 import com.lollipop.wifip2p.stp.data.DataBody
 import java.io.File
+import java.io.FileOutputStream
 
 class AutoSaveReadListener(
-    val fileDir: File,
+    private val fileDir: File,
     val infoListener: (ReadResult) -> Unit
 ) : StpHandler.ReaderListener {
 
@@ -12,16 +13,50 @@ class AutoSaveReadListener(
     override fun onRead(data: DataBody) {
         when (data) {
             is DataBody.File -> {
-                TODO()
+                try {
+                    val file = getFile(data.getFileName())
+                    file.parentFile?.mkdirs()
+                    val outputStream = FileOutputStream(file)
+                    data.writeTo(outputStream)
+                    outputStream.flush()
+                    outputStream.close()
+                    infoListener(ReadResult.FileInfo(file))
+                } catch (e: Throwable) {
+                    infoListener(ReadResult.Error(e))
+                }
             }
-            is DataBody.HeartBeat -> TODO()
-            is DataBody.Text -> TODO()
+
+            is DataBody.HeartBeat -> {
+                infoListener(ReadResult.HeartBeatInfo(data.getText()))
+            }
+
+            is DataBody.Text -> {
+                infoListener(ReadResult.TextInfo(data.getText()))
+            }
         }
+    }
+
+    private fun getFile(name: String): File {
+        var file = File(fileDir, name)
+        while (file.exists()) {
+            val fileName = file.name
+            val lastIndexDot = fileName.lastIndexOf(".")
+            if (lastIndexDot >= 0) {
+                val oldName = fileName.substring(0, lastIndexDot)
+                val newName = "${oldName}-1.${fileName.substring(lastIndexDot + 1)}"
+                file = File(fileDir, newName)
+            } else {
+                file = File(fileDir, "$fileName-1")
+            }
+        }
+        return file
     }
 
     sealed class ReadResult {
 
         class TextInfo(val text: String) : ReadResult()
+
+        class HeartBeatInfo(val text: String) : ReadResult()
 
         class FileInfo(val file: File) : ReadResult()
 
