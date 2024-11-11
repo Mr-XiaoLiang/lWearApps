ca5eb5b73911fb8d8ab59208026a03983cbddfa2
lollipop <liujian594@hotmail.com>
2024-11-11 23:04:19 +0800

    feat: 打算做游戏日志，对于数据库的Base有点想法，嘿嘿嘿

--- basic/src/main/java/com/lollipop/wear/data/DatabaseHelper.kt
+++ basic/src/main/java/com/lollipop/wear/data/DatabaseHelper.kt
@@ -232,14 +232,4 @@ abstract class DatabaseHelper(
         TEXT("TEXT"), INTEGER("INTEGER"), DOUBLE("REAL")
     }
 
-
-    object FeedReaderContract {
-        // Table contents are grouped together in an anonymous object.
-        object FeedEntry : BaseColumns {
-            const val TABLE_NAME = "entry"
-            const val COLUMN_NAME_TITLE = "title"
-            const val COLUMN_NAME_SUBTITLE = "subtitle"
-        }
-    }
-
 }
