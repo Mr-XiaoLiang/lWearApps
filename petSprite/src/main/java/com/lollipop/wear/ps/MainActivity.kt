62bf78a3805b774521065b73c9cac1710bdd8320
lollipop <liujian594@hotmail.com>
2024-11-19 22:17:12 +0800

    feat: 每天写一点，接下来是写放大动画效果

--- petSprite/src/main/java/com/lollipop/wear/ps/MainActivity.kt
+++ petSprite/src/main/java/com/lollipop/wear/ps/MainActivity.kt
@@ -1,7 +1,10 @@
 package com.lollipop.wear.ps
 
+import android.animation.Animator
+import android.animation.ValueAnimator
 import android.os.Bundle
 import android.util.Log
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import com.lollipop.wear.ps.business.MainDashboardDelegate
 import com.lollipop.wear.ps.databinding.ActivityMainBinding
@@ -43,6 +46,61 @@ class MainActivity : AppCompatActivity() {
         } else {
             player.changedToward(toward, true)
         }
+    }
+
+    private class ContentPanelController(
+        private val contentPanel: View,
+        private val contentPanelBackground: View,
+        private val openButton: View
+    ) : ValueAnimator.AnimatorUpdateListener, Animator.AnimatorListener {
+
+        companion object {
+            private const val ANIMATOR_DURATION = 300L
+            private const val ANIMATOR_VALUE_MIN = 0F
+            private const val ANIMATOR_VALUE_MAX = 1F
+        }
+
+        private val valueAnimator by lazy {
+            ValueAnimator().apply {
+                addUpdateListener(this@ContentPanelController)
+                addListener(this@ContentPanelController)
+            }
+        }
+
+        init {
+            openButton.setOnClickListener {
+                open()
+            }
+        }
+
+        fun close() {
+            TODO("Not yet implemented")
+        }
+
+        fun open() {
+            TODO("Not yet implemented")
+        }
+
+        override fun onAnimationUpdate(animation: ValueAnimator) {
+            TODO("Not yet implemented")
+        }
+
+        override fun onAnimationStart(animation: Animator) {
+            TODO("Not yet implemented")
+        }
+
+        override fun onAnimationEnd(animation: Animator) {
+            TODO("Not yet implemented")
+        }
+
+        override fun onAnimationCancel(animation: Animator) {
+            TODO("Not yet implemented")
+        }
+
+        override fun onAnimationRepeat(animation: Animator) {
+            TODO("Not yet implemented")
+        }
+
     }
 
 }
