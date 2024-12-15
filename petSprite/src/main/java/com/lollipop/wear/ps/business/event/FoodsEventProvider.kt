e4dcaeac56e477aab854ff19a1c80c0597d2b985
lollipop <liujian594@hotmail.com>
2024-12-15 20:57:45 +0800

    feat: 做食物的逻辑

--- petSprite/src/main/java/com/lollipop/wear/ps/business/event/FoodsEventProvider.kt
+++ petSprite/src/main/java/com/lollipop/wear/ps/business/event/FoodsEventProvider.kt
@@ -2,12 +2,54 @@ package com.lollipop.wear.ps.business.event
 
 import com.lollipop.wear.ps.engine.GameController
 import com.lollipop.wear.ps.engine.state.GameSomeThings
+import com.lollipop.wear.ps.engine.state.GameState
+import com.lollipop.wear.ps.engine.state.IntGameState
+import com.lollipop.wear.ps.engine.state.impl.HealthState
+import com.lollipop.wear.ps.engine.state.impl.MoodState
+import com.lollipop.wear.ps.engine.state.impl.SatiationState
 
 object FoodsEventProvider : GameController.RandomThingsProvider {
 
     override val weight: Int = 100
 
+    private val stateArray by lazy {
+        arrayOf(HealthState, MoodState, SatiationState)
+    }
+
     override fun getThings(): GameSomeThings? {
+        val state = findFocusState() ?: return null
+        when (state) {
+            HealthState -> {
+                TODO()
+            }
+
+            MoodState -> {
+                TODO()
+            }
+
+            SatiationState -> {
+                TODO()
+            }
+        }
         return null
     }
+
+    private fun findFocusState(): GameState? {
+        return findMinState(stateArray)
+    }
+
+    private fun findMinState(array: Array<IntGameState>): GameState? {
+        if (array.isEmpty()) {
+            return null
+        }
+        var min = array[0]
+        for (i in 1 until array.size) {
+            val state = array[i]
+            if (state.current < min.current) {
+                min = state
+            }
+        }
+        return min
+    }
+
 }
