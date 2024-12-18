06061598bb510589f2b510ac1558601e541c6aba
lollipop <liujian594@hotmail.com>
2024-12-18 22:12:09 +0800

    feat: 增加幸运事件。但是现在不想写。我想装Linux

---
+++ petSprite/src/main/java/com/lollipop/wear/ps/business/options/Lucky.kt
@@ -0,0 +1,11 @@
+package com.lollipop.wear.ps.business.options
+
+import com.lollipop.wear.ps.engine.state.GameOption
+
+object Lucky: OptionList {
+
+    override val options: Array<GameOption>
+        get() = TODO("Not yet implemented")
+
+
+}
