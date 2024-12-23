27e60ce84d92b22fac8392a1940d8455635bbd97
lollipop <liujian594@hotmail.com>
2024-12-23 22:05:45 +0800

    feat: 接下来是可以考虑开始做动画的播放了

--- petSprite/src/main/java/com/lollipop/wear/ps/business/event/ToysEventProvider.kt
+++ petSprite/src/main/java/com/lollipop/wear/ps/business/event/ToysEventProvider.kt
@@ -2,9 +2,14 @@ package com.lollipop.wear.ps.business.event
 
 import com.lollipop.wear.ps.business.options.Toys
 import com.lollipop.wear.ps.engine.GameController
+import com.lollipop.wear.ps.engine.state.GameOption
 import com.lollipop.wear.ps.engine.state.GameOptionAction
 import com.lollipop.wear.ps.engine.state.GameOptionReason
 import com.lollipop.wear.ps.engine.state.GameSomeThings
+import com.lollipop.wear.ps.engine.state.impl.RichState
+import com.lollipop.wear.ps.engine.state.impl.SatiationState
+import com.lollipop.wear.ps.engine.state.type.Food
+import com.lollipop.wear.ps.engine.state.type.Money
 
 /**
  * 玩耍相关的事件
@@ -12,6 +17,31 @@ import com.lollipop.wear.ps.engine.state.GameSomeThings
 object ToysEventProvider : GameController.SimpleRandomThingsProvider(GameController.WEIGHT_HIGH) {
 
     override fun getThings(): GameSomeThings? {
-        return randomThings(Toys.options, GameOptionReason.Lucky, GameOptionAction.GOT)
+        val srcOptions = Toys.options
+        val activeOptions = ArrayList<GameOption>()
+        val richValue = RichState.current
+        val satiationState = SatiationState.current
+        for (option in srcOptions) {
+            if (option is Money) {
+                val amount = option.amount
+                if (amount < 0 && richValue + amount < 0) {
+                    // 如果需要付费，并且余额不足，那么放弃运动
+                    continue
+                }
+            }
+            if (option is Food) {
+                val kcal = option.kcal
+                if (kcal < 0 && satiationState + kcal < 0) {
+                    // 如果需要消耗能量，并且能量不足，那么放弃运动
+                    continue
+                }
+            }
+            activeOptions.add(option)
+        }
+        return randomThings(
+            activeOptions.toTypedArray(),
+            GameOptionReason.Lucky,
+            GameOptionAction.GOT
+        )
     }
 }
--- petSprite/src/main/java/com/lollipop/wear/ps/business/options/Toys.kt
+++ petSprite/src/main/java/com/lollipop/wear/ps/business/options/Toys.kt
@@ -1,11 +1,130 @@
 package com.lollipop.wear.ps.business.options
 
+import com.lollipop.wear.ps.R
 import com.lollipop.wear.ps.engine.state.GameOption
+import com.lollipop.wear.ps.engine.state.type.Food
+import com.lollipop.wear.ps.engine.state.type.Money
+import com.lollipop.wear.ps.engine.state.type.Toy
 
 object Toys : OptionList {
 
+    abstract class ToyOption(
+        override val key: String,
+        override val name: Int,
+        override val dopamine: Int,
+        override val amount: Int,
+        override val kcal: Int
+    ) : GameOption, Toy, Money, Food
 
+    /**
+     * 发呆
+     */
+    object StareBlankly : ToyOption(
+        key = "toy_stare_blankly",
+        name = R.string.label_toy_stare_blankly,
+        dopamine = 10,
+        amount = 0,
+        kcal = -10
+    )
 
-    override val options: Array<GameOption> = arrayOf()
+    /**
+     * 乒乓球
+     */
+    object PingPong : ToyOption(
+        key = "toy_ping_pong",
+        name = R.string.label_toy_ping_pong,
+        dopamine = 30,
+        amount = 0,
+        kcal = -30
+    )
+
+    /**
+     * 足球
+     */
+    object Football : ToyOption(
+        key = "toy_football",
+        name = R.string.label_toy_football,
+        dopamine = 40,
+        amount = 0,
+        kcal = -40
+    )
+
+    /**
+     * 篮球
+     */
+    object Basketball : ToyOption(
+        key = "toy_basketball",
+        name = R.string.label_toy_basketball,
+        dopamine = 40,
+        amount = 0,
+        kcal = -40
+    )
+
+    /**
+     * 羽毛球
+     */
+    object Badminton : ToyOption(
+        key = "toy_badminton",
+        name = R.string.label_toy_badminton,
+        dopamine = 40,
+        amount = 0,
+        kcal = -40
+    )
+
+    /**
+     * 网球
+     */
+    object Tennis : ToyOption(
+        key = "toy_tennis",
+        name = R.string.label_toy_tennis,
+        dopamine = 40,
+        amount = 0,
+        kcal = -40
+    )
+
+    /**
+     * 保龄球
+     */
+    object Bowling : ToyOption(
+        key = "toy_bowling",
+        name = R.string.label_toy_bowling,
+        dopamine = 40,
+        amount = -100,
+        kcal = -40
+    )
+
+    /**
+     * 高尔夫球
+     */
+    object Golf : ToyOption(
+        key = "toy_golf",
+        name = R.string.label_toy_golf,
+        dopamine = 40,
+        amount = -1000,
+        kcal = -40
+    )
+
+    /**
+     * 游泳
+     */
+    object Swimming : ToyOption(
+        key = "toy_swimming",
+        name = R.string.label_toy_swimming,
+        dopamine = 40,
+        amount = -10,
+        kcal = -40
+    )
+
+    override val options: Array<GameOption> = arrayOf(
+        StareBlankly,
+        PingPong,
+        Football,
+        Basketball,
+        Badminton,
+        Tennis,
+        Bowling,
+        Golf,
+        Swimming
+    )
 
 }
