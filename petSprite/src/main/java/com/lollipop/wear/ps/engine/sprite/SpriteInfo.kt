d24ac592cdf9a94db28c1bfe522c2623b17059e9
lollipop <liujian594@hotmail.com>
2024-12-27 23:35:55 +0800

    feat: SpriteInfo 的序列化和反序列化做好了。

--- petSprite/src/main/java/com/lollipop/wear/ps/engine/sprite/SpriteInfo.kt
+++ petSprite/src/main/java/com/lollipop/wear/ps/engine/sprite/SpriteInfo.kt
@@ -8,6 +8,7 @@ import org.json.JSONObject
 import java.io.File
 
 sealed class SpriteInfo(
+    private val type: String,
     val left: SpriteFrame,
     val up: SpriteFrame,
     val right: SpriteFrame,
@@ -17,6 +18,22 @@ sealed class SpriteInfo(
     var name = ""
 
     abstract fun loadBitmap(context: Context): Bitmap?
+
+    protected abstract fun toContentJson(): String
+
+    protected abstract fun isContentSame(other: SpriteInfo): Boolean
+
+    fun toJson(): JSONObject {
+        val json = JSONObject()
+        json.put(KEY_LEFT_FRAME, left.toJson())
+        json.put(KEY_UP_FRAME, up.toJson())
+        json.put(KEY_RIGHT_FRAME, right.toJson())
+        json.put(KEY_DOWN_FRAME, down.toJson())
+        json.put(KEY_TYPE, type)
+        json.put(KEY_CONTENT, toContentJson())
+        json.put(KEY_NAME, name)
+        return json
+    }
 
     fun getFrame(toward: SpriteToward): SpriteFrame {
         when (toward) {
@@ -55,9 +72,8 @@ sealed class SpriteInfo(
         return isContentSame(other)
     }
 
-    protected abstract fun isContentSame(other: SpriteInfo): Boolean
-
     data object None : SpriteInfo(
+        "",
         SpriteFrame.None,
         SpriteFrame.None,
         SpriteFrame.None,
@@ -65,6 +81,10 @@ sealed class SpriteInfo(
     ) {
         override fun loadBitmap(context: Context): Bitmap? {
             return null
+        }
+
+        override fun toContentJson(): String {
+            return ""
         }
 
         override fun isContentSame(other: SpriteInfo): Boolean {
@@ -79,7 +99,13 @@ sealed class SpriteInfo(
         up: SpriteFrame,
         right: SpriteFrame,
         down: SpriteFrame
-    ) : SpriteInfo(left, up, right, down) {
+    ) : SpriteInfo(TYPE_ASSETS, left, up, right, down) {
+
+        companion object {
+            fun parseContent(value: String): String {
+                return value
+            }
+        }
 
         override fun loadBitmap(context: Context): Bitmap? {
             try {
@@ -88,6 +114,10 @@ sealed class SpriteInfo(
                 Log.e("SpriteInfo", "FromAssets.loadBitmap", e)
             }
             return null
+        }
+
+        override fun toContentJson(): String {
+            return path
         }
 
         override fun isContentSame(other: SpriteInfo): Boolean {
@@ -99,37 +129,20 @@ sealed class SpriteInfo(
 
     }
 
-    class FromResource(
-        private val id: Int,
-        left: SpriteFrame,
-        up: SpriteFrame,
-        right: SpriteFrame,
-        down: SpriteFrame
-    ) : SpriteInfo(left, up, right, down) {
-        override fun loadBitmap(context: Context): Bitmap? {
-            try {
-                return BitmapCache.optResource(context, id)
-            } catch (e: Throwable) {
-                Log.e("SpriteInfo", "FromResource.loadBitmap", e)
-            }
-            return null
-        }
-
-        override fun isContentSame(other: SpriteInfo): Boolean {
-            if (other is FromResource) {
-                return other.id == this.id
-            }
-            return false
-        }
-    }
-
     class FromFile(
         private val file: File,
         left: SpriteFrame,
         up: SpriteFrame,
         right: SpriteFrame,
         down: SpriteFrame
-    ) : SpriteInfo(left, up, right, down) {
+    ) : SpriteInfo(TYPE_FILE, left, up, right, down) {
+
+        companion object {
+            fun parseContent(value: String): File {
+                return File(value)
+            }
+        }
+
         override fun loadBitmap(context: Context): Bitmap? {
             try {
                 return BitmapCache.optFile(file)
@@ -137,6 +150,10 @@ sealed class SpriteInfo(
                 Log.e("SpriteInfo", "FromFile.loadBitmap", e)
             }
             return null
+        }
+
+        override fun toContentJson(): String {
+            return file.path
         }
 
         override fun isContentSame(other: SpriteInfo): Boolean {
@@ -153,7 +170,14 @@ sealed class SpriteInfo(
         up: SpriteFrame,
         right: SpriteFrame,
         down: SpriteFrame
-    ) : SpriteInfo(left, up, right, down) {
+    ) : SpriteInfo(TYPE_BYTE_ARRAY, left, up, right, down) {
+
+        companion object {
+            fun parseContent(value: String): ByteArray {
+                return value.toByteArray(Charsets.UTF_8)
+            }
+        }
+
         override fun loadBitmap(context: Context): Bitmap? {
             try {
                 return BitmapCache.optByteArray(bytes)
@@ -161,6 +185,10 @@ sealed class SpriteInfo(
                 Log.e("SpriteInfo", "FromByteArray.loadBitmap", e)
             }
             return null
+        }
+
+        override fun toContentJson(): String {
+            return String(bytes, Charsets.UTF_8)
         }
 
         override fun isContentSame(other: SpriteInfo): Boolean {
@@ -172,6 +200,65 @@ sealed class SpriteInfo(
     }
 
     companion object {
+
+        private const val KEY_LEFT_FRAME = "left_frame"
+        private const val KEY_UP_FRAME = "up_frame"
+        private const val KEY_RIGHT_FRAME = "right_frame"
+        private const val KEY_DOWN_FRAME = "down_frame"
+        private const val KEY_TYPE = "type"
+        private const val KEY_CONTENT = "content"
+        private const val KEY_NAME = "name"
+
+
+        const val TYPE_ASSETS = "assets"
+        const val TYPE_FILE = "file"
+        const val TYPE_BYTE_ARRAY = "byteArray"
+
+        fun parse(json: JSONObject): SpriteInfo {
+            val type = json.optString(KEY_TYPE, "")
+            val info = when (type) {
+                TYPE_ASSETS -> {
+                    FromAssets(
+                        path = FromAssets.parseContent(json.optString(KEY_CONTENT, "")),
+                        left = parseFrame(json, KEY_LEFT_FRAME),
+                        up = parseFrame(json, KEY_UP_FRAME),
+                        right = parseFrame(json, KEY_RIGHT_FRAME),
+                        down = parseFrame(json, KEY_DOWN_FRAME)
+                    )
+                }
+
+                TYPE_FILE -> {
+                    FromFile(
+                        file = FromFile.parseContent(json.optString(KEY_CONTENT, "")),
+                        left = parseFrame(json, KEY_LEFT_FRAME),
+                        up = parseFrame(json, KEY_UP_FRAME),
+                        right = parseFrame(json, KEY_RIGHT_FRAME),
+                        down = parseFrame(json, KEY_DOWN_FRAME)
+                    )
+                }
+
+                TYPE_BYTE_ARRAY -> {
+                    FromByteArray(
+                        bytes = FromByteArray.parseContent(json.optString(KEY_CONTENT, "")),
+                        left = parseFrame(json, KEY_LEFT_FRAME),
+                        up = parseFrame(json, KEY_UP_FRAME),
+                        right = parseFrame(json, KEY_RIGHT_FRAME),
+                        down = parseFrame(json, KEY_DOWN_FRAME)
+                    )
+                }
+
+                else -> {
+                    return None
+                }
+            }
+            info.name = json.optString(KEY_NAME, "")
+            return info
+        }
+
+        private fun parseFrame(json: JSONObject, key: String): SpriteFrame {
+            return SpriteFrame.parse(json.optJSONObject(key) ?: JSONObject())
+        }
+
         inline fun <reified T : SpriteInfo> createBy4x4(
             imageWidth: Int,
             imageHeight: Int = imageWidth,
