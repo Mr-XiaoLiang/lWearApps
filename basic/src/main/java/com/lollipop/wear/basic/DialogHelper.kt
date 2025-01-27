fba6a0fced7f2e86807ee55cbbb230025bf069cf
lollipop <liujian594@hotmail.com>
2025-01-27 20:58:21 +0800

    feat: 做了几个通用的Dialog辅助工具

---
+++ basic/src/main/java/com/lollipop/wear/basic/DialogHelper.kt
@@ -0,0 +1,69 @@
+package com.lollipop.wear.basic
+
+import android.content.Context
+import android.content.DialogInterface
+import android.view.Gravity
+import android.widget.FrameLayout
+import android.widget.LinearLayout
+import android.widget.ProgressBar
+import androidx.appcompat.app.AlertDialog
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
+
+
+object DialogHelper {
+
+    fun list(
+        context: Context,
+        titleRes: Int,
+        itemResList: Array<Int>,
+        onSelect: (DialogInterface, Int) -> Unit
+    ): AlertDialog {
+        val itemValueList = itemResList.map { context.getString(it) }.toTypedArray()
+        return MaterialAlertDialogBuilder(context)
+            .setTitle(titleRes)
+            .setItems(itemValueList) { dialog, which ->
+                onSelect(dialog, itemResList[which])
+            }
+            .show()
+    }
+
+    fun loading(
+        context: Context,
+        titleRes: Int,
+        cancelable: Int = 0,
+        onCancel: (() -> Unit)? = null
+    ): AlertDialog {
+        return MaterialAlertDialogBuilder(context)
+            .setTitle(titleRes)
+            .setView(LinearLayout(context).also { linearLayout ->
+                linearLayout.orientation = LinearLayout.VERTICAL
+                val progressBar = ProgressBar(
+                    context,
+                    null,
+                    android.R.attr.progressBarStyleHorizontal
+                )
+                linearLayout.addView(
+                    progressBar,
+                    LinearLayout.LayoutParams.MATCH_PARENT,
+                    LinearLayout.LayoutParams.WRAP_CONTENT
+                )
+                linearLayout.layoutParams = FrameLayout.LayoutParams(
+                    LinearLayout.LayoutParams.MATCH_PARENT,
+                    LinearLayout.LayoutParams.WRAP_CONTENT
+                )
+                linearLayout.gravity = Gravity.CENTER
+                progressBar.isIndeterminate = true
+            })
+            .setCancelable(false)
+            .also { builder ->
+                if (cancelable != 0) {
+                    builder.setNegativeButton(cancelable) { dialog, _ ->
+                        onCancel?.invoke()
+                        dialog.dismiss()
+                    }
+                }
+            }
+            .show()
+    }
+
+}
