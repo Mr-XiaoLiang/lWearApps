2bbdbf425c005c92084e85184c21e2093ed80d9b
lollipop <liujian594@hotmail.com>
2025-03-03 23:53:22 +0800

    feat: 增加个新模块，先弄好玩的

---
+++ kTouch/src/main/java/com/lollipop/ktouch/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.lollipop.ktouch
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.lollipop.ktouch.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private val binding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+    }
+
+}
